d2ffe20ed5287037cce812d97a51da1d7ce11884
sohyun95277 <sohyun95277@gmail.com>
2023-12-14 09:32:16 +0900

    feat(LottoSeller): 랜덤한 수 6개를 생성하는 기능

---
+++ src/main/java/lotto/domain/LottoSeller.java
@@ -0,0 +1,43 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class LottoSeller {
+    private static final int LOTTO_PRICE = 1_000;
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 45;
+    private static final int NUMBER_COUNT = 6;
+    private final int money;
+
+    public LottoSeller(String money) {
+        this.money = getValidMoney(money);
+    }
+
+    // 숫자로만 이루어져있는지, 1000원으로 나누어 떨어지는지
+    private int getValidMoney(String input) {
+        validateOnlyDigit(input);
+        int money = Integer.parseInt(input);
+        validateHasRemain(money);
+
+        return money;
+    }
+
+    public List<Integer> generateLotto() {
+        return Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, NUMBER_COUNT);
+    }
+
+
+    private void validateOnlyDigit(String input) {
+        if (!input.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자만 입력해주세요.");
+        }
+    }
+
+    private void validateHasRemain(int money) {
+        if (money % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.");
+        }
+    }
+
+}

c11aebe96cc05cddb95bde741e0d143b69c95c5c
sohyun95277 <sohyun95277@gmail.com>
2023-12-14 11:01:46 +0900

    feat(OutputView): 당첨내역 출력기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,16 +1,51 @@
 package lotto.view;
 
+import java.text.NumberFormat;
 import java.util.Collections;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import lotto.domain.Lotto;
+import lotto.domain.Prize;
 
 public class OutputView {
     private static final String PURCHASE_COUNT = "%d개를 구매했습니다.";
     private static final String PRIZE_RESULT = "당첨 통계";
     private static final String LINE_SEPARATOR = System.lineSeparator();
     private static final String RESULT_DETAIL = "%d개 일치 (%s원) - %d개";
-    private static final String BONUS_DETAIL = "%d개 일치, 보너스 볼 일치 (%s원) - %개";
+    private static final String BONUS_DETAIL = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
     private static final String PROFIT_RESULT = "총 수익률은 %%입니다.";
+
+
+    public void printSecondPrizeResult(int matchCount, long prizeMoney, int count) {
+        String message = String.format(BONUS_DETAIL, matchCount, moneyFormatter(prizeMoney), count);
+        System.out.println(message);
+    }
+
+    public void printPrizeResult(EnumMap<Prize, Integer> prizeResult) {
+        printResultTitle();
+        for (Map.Entry<Prize, Integer> pair : prizeResult.entrySet()) {
+            String message = String.format(RESULT_DETAIL, pair.getKey().getMatchCount(),
+                    moneyFormatter(pair.getKey().getPrizeMoney()), pair.getValue());
+            if (pair.getKey() == Prize.SECOND) {
+                message = String.format(BONUS_DETAIL, pair.getKey().getMatchCount(),
+                        moneyFormatter(pair.getKey().getPrizeMoney()), pair.getValue());
+            }
+            if (pair.getKey() != Prize.NONE) {
+                System.out.println(message);
+            }
+        }
+    }
+
+    private String moneyFormatter(long prizeMoney) {
+        NumberFormat format = NumberFormat.getInstance(Locale.KOREA);
+        return format.format(prizeMoney);
+    }
+
+    public void printResultTitle() {
+        System.out.println(PRIZE_RESULT);
+    }
 
     public void printPurchaseResult(List<Lotto> tickets, int count) {
         printPurchaseCount(count);

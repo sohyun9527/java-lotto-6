e921228fba1a7a0d0bef2c8c050d1a14255d022f
sohyun95277 <sohyun95277@gmail.com>
2023-12-14 10:42:36 +0900

    feat(Prize): 2등과 3등을 판별하는 기능
    5개 -> 2등
    보너스 볼을 포함하지 않았다면 3등 반환

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -5,7 +5,8 @@ public enum Prize {
     SECOND(5, 30_000_000),
     THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
-    FIFTH(3, 5_000);
+    FIFTH(3, 5_000),
+    NONE(0, 0);
 
     private final int matchCount;
     private final long prizeMoney;
@@ -13,6 +14,23 @@ public enum Prize {
     Prize(int matchCount, long prizeMoney) {
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
+    }
+
+    public static Prize getPrize(int matchCount, boolean isSecond) {
+        Prize prize = findByMatchCount(matchCount);
+        if (prize == SECOND && !isSecond) {
+            return THIRD;
+        }
+        return prize;
+    }
+
+    public static Prize findByMatchCount(int matchCount) {
+        for (Prize prize : Prize.values()) {
+            if (prize.matchCount == matchCount) {
+                return prize;
+            }
+        }
+        return NONE;
     }
 
     public int getMatchCount() {

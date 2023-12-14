5b3e36a66f254fa5b33351340d9f758b159fc62c
sohyun95277 <sohyun95277@gmail.com>
2023-12-14 11:22:21 +0900

    feat(ReadUntilValid): 올바른 값을 받을 때까지 재실행 기능

---
+++ src/main/java/lotto/util/ReadUntilValid.java
@@ -0,0 +1,16 @@
+package lotto.util;
+
+import java.util.function.Supplier;
+
+public class ReadUntilValid {
+
+    public static <T> T readUntilValidInput(Supplier<T> inputFunction) {
+        while (true) {
+            try {
+                return inputFunction.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}

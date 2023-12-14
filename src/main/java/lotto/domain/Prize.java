4b51b317f952fdc44b4368ffa2a1302ca06d218c
sohyun95277 <sohyun95277@gmail.com>
2023-12-14 10:01:12 +0900

    feat(Prize): 당첨 결과를 담은 enum

---
+++ src/main/java/lotto/domain/Prize.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+public enum Prize {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000);
+
+    private final int matchCount;
+    private final long prizeMoney;
+
+    Prize(int matchCount, long prizeMoney) {
+        this.matchCount = matchCount;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public long getPrizeMoney() {
+        return prizeMoney;
+    }
+}
